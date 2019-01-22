8bacd193702757ffdc1df9e9462a7f8ff6b87cc7
vinnyA3 <vincent.aceto@gmail.com>
2019-01-21 20:53:33 -0500

    feat(message): add conditional for message render

--- client/src/components/message.js
+++ client/src/components/message.js
@@ -1,4 +1,8 @@
 import React from 'react';
 import styles from './message-styles.scss';
 
-export default ({ message }) => <h1 className={styles.message}> {message} </h1>;
+export default ({ message }) => (
+  <h1 className={styles.message}>
+    {!message ? '' : JSON.stringify(message, null, 2)}
+  </h1>
+);
