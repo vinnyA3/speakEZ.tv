3f9ad6a2a94471dfbed0dd8bb3184512bd482b93
vinnyA3 <vincent.aceto@gmail.com>
2019-01-21 22:44:43 -0500

    feat(index): consolidate app + index

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
-import App from 'app';
+import { ajax } from 'rxjs/ajax';
+import { map, catchError } from 'rxjs/operators';
+import { prop } from 'ramda';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: null,
+    };
+  }
+
+  componentDidMount() {
+    ajax
+      .getJSON('/api/messages')
+      .pipe(
+        map(prop('results')),
+        catchError(e => console.error(e))
+      )
+      .subscribe(messages => this.setState({ messages }));
+  }
+
+  render() {
+    const renderMessages = () =>
+      this.state.messages.map(message => {
+        return (
+          <div key={message.objectId}>
+            <h2>{message.username}</h2>
+            <p>{message.text}</p>
+          </div>
+        );
+      });
+
+    return <div>{!this.state.messages ? '' : renderMessages()}</div>;
+  }
+}
 
 render(<App />, document.getElementById('app'));
 
